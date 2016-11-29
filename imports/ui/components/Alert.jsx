0328d11a0552aa654ff58798f0b0857c406e6762
fabienoger <fab-oger@live.fr>
2016-11-30 00:01:39 +0100

    Close Alert

--- imports/ui/components/Alert.jsx
+++ imports/ui/components/Alert.jsx
@@ -2,10 +2,22 @@ import React, {PropTypes} from 'react';
 import { Meteor }         from 'meteor/meteor';
 
 export default class Alert extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+  }
+  close(e) {
+    this.setState({display: false})
+  }
   render() {
+    if (!this.state.display) {
+      return (<div></div>);
+    }
     return (
       <div className={"toast toast-" + this.props.type}>
-        <button className="btn btn-clear float-right"></button>
+        <button onClick={this.close.bind(this)} className="btn btn-clear float-right"></button>
         <i className="fa fa-exclamation-triangle"></i>&nbsp;
         {this.props.message}
       </div>
