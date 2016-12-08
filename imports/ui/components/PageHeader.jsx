1106717af8fe634fd384b547c517b8128fc680d5
fabienoger <fab-oger@live.fr>
2016-12-08 20:50:53 +0100

    Add presentation text and custom page header

--- imports/ui/components/PageHeader.jsx
+++ imports/ui/components/PageHeader.jsx
@@ -7,24 +7,27 @@ export default class PageHeader extends React.Component {
     Scroll.animateScroll.scrollToBottom();
   }
   render() {
-    const heightFifty = {
-      height: "50%"
-    }
     return (
       <div id="page-header">
-        <div className="backgroundImg" style={heightFifty}>
+        <div className="backgroundImg" style={{height: '30%'}}>
           <img className="background" src="/images/home_background.png" />
           <div className="logo-wrapper">
             <img src="/images/logo-MTN.svg" />
           </div>
         </div>
-        <div style={heightFifty} className="presentation">
-          <h1 className="text-center">Présentation</h1>
-          <div className="divider" style={{width: "25%", margin: "auto"}}></div>
-          <div style={{width: "80%", padding: "25px 50px"}} className="centered text">
-            <p className="text-center">A chaque métier sa formation ! Avec la Grande École du Numérique, de nombreuses formations accessibles à tou.te.s se sont ouvertes en France, que vous ayez des diplômes ou pas.</p>
-            <p className="text-center">Il existe quelques sites les répertoriant, mais peu permettent de se repérer en un coup d’oeil.</p>
-            <p className="text-center">C’est l’objectif principal de notre plateforme : vous renseigner, rendre très visuel la localisation des formations Grande École du Numérique (GEN) et vous montrer les opportunités de travail qu’elles peuvent vous apporter.</p>
+        <div style={{height: '70%'}} className="presentation-wrapper">
+          <div className="presentation">
+            <h1 className="text-center">Présentation</h1>
+            <div className="divider" style={{width: "25%", margin: "auto"}}></div>
+            <div style={{width: "80%", padding: "25px 50px"}} className="centered text">
+              <p className="text-center">A chaque métier sa formation ! Avec la Grande École du Numérique, de nombreuses formations accessibles à tou.te.s se sont ouvertes en France, que vous ayez des diplômes ou pas.</p>
+              <p className="text-center">Il existe quelques sites les répertoriant, mais peu permettent de se repérer en un coup d’oeil.</p>
+              <p className="text-center">C’est l’objectif principal de notre plateforme : vous renseigner, rendre très visuel la localisation des formations Grande École du Numérique (GEN) et vous montrer les opportunités de travail qu’elles peuvent vous apporter.</p>
+            </div>
+          </div>
+          <div className="download-wrapper">
+            <h2 className="text-center">C'est quoi ce taff ?</h2>
+            <p className="text-center">Vous êtes attiré par un métier dans le numérique mais vous ne savez pas exactement en quoi il consiste ? Pas de souci, on a tout prévu :)</p>
           </div>
           <div className="centered arrow-wrapper" ref="arrowWrapper" onClick={this.scrollDown.bind(this)}>
             <i className="fa fa-angle-double-down fa-3x" aria-hidden="true"></i>
